381635416e3be78df1e512e55d3aa1651e3fbec9
Ubuntu <ubuntu@ip-172-31-35-110.ap-south-1.compute.internal>
2020-03-16 04:53:09 +0000

    work done

--- demo.sh
+++ demo.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 sudo  apt-get update
+sudo apt-get install python3-pip
 sudo apt-get install nginx
 sudo apt-get install supervisor
-sudo pip3 install gunicorn 
-sudo cp ~/Desktop/gunicorn.conf  /etc/supervisor/conf.d/gunicorn.conf
+sudo pip3 install gunicorn
+sudo pip3 install -r requirements.txt 
+sudo cp ~/hello/gunicorn.conf  /etc/supervisor/conf.d/gunicorn.conf
 cd /etc/supervisor/conf.d
 sudo mkdir  /var/log/gunicorn
 cd
-cd Desktop
+cd hello
 sudo supervisorctl reread
 sudo supervisorctl update
 cd /etc/nginx/sites-available
-sudo cp ~/Desktop/django.conf  /etc/nginx/sites-available/django.conf
+sudo cp ~/hello/django.conf  /etc/nginx/sites-available/django.conf
 sudo ln django.conf /etc/nginx/sites-enabled
 cd
-cd Desktop
+cd hello
 sudo nginx -t
 sudo service nginx reload
 sudo service nginx restart
